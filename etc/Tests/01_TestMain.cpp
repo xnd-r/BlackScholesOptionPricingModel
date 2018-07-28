e453254ec39170fdba422ca30a6cabecdad0cffa
Alexander Romanov <al.romanov2012@yandex.ru>
2018-07-28 03:24:19 +0300

    changed gitignore

--- etc/Tests/01_TestMain.cpp
+++ etc/Tests/01_TestMain.cpp
@@ -1,8 +1,9 @@
 // Copyright 2018 Romanov Alexander
-
+#include <conio.h>
 #include "../../3rdparty/gtest/gtest.h"
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
+	getch();
 }
